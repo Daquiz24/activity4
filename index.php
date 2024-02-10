51756fa6ca6800ec234936bd72ea0cb40ae72575
pipao <limcriss78@gmail.com>
2024-02-10 18:26:22 +0800

    added space

--- index.php
+++ index.php
@@ -172,7 +172,7 @@ if ($result->num_rows > 0) {
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="loginModalLabel">Login</h5>
+                <h5 class="modal-title" id="loginModalLabel">login</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
